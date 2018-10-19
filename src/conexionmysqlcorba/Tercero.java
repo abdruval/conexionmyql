b1195a3b57b9b26084b6c9d7c4519501ca5d96a2
ABDRUVAL <ABDRUVAL@LAPTOP-G60JNQQ2>
2018-10-18 22:07:16 -0500

    Se implementó los metodos de actualizar, guardar.

--- src/conexionmysqlcorba/Tercero.java
+++ src/conexionmysqlcorba/Tercero.java
@@ -1,22 +1,111 @@
+
 package conexionmysqlcorba;
-import Terceros.*;
+
 import java.sql.*;
 import javax.swing.JOptionPane;
+import Terceros.*;
 
-/**
- *
- * @author Abdruval Collazos
- */
-public class Tercero extends TerceroPOA{
+public class Tercero extends TerceroPOA {
+    Conexion objConec = new Conexion();
+
+    @Override
+    public boolean insertartercero(String nombres, String apellidos, int telefono) {
+        boolean resultado = false;
+        
+        try {
+            String sql= "insert into terceros (nombres,apellidos,telefono)values('"+nombres+"','"+apellidos+"','"+telefono+"')";
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            int valor = st.executeUpdate(sql);
+            if (valor>0){
+                resultado = true;
+            }
+            objConec.conex.close();
+            st.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public boolean actualizartercero(int id, String nombres, String apellidos, int telefono) {
+         boolean resultado = false;
+        
+        try {
+            String sql = "update tercero set nombres = '"+nombres+"',apellidos = '"+apellidos+"',telefono = '"+telefono+"' where id = '"+id+"'";
+           
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public boolean eliminartercero(int id) {
+        
+         boolean resultado = false;
+        
+        try {
+            String sql = "Delete from terceros where id = "+id;
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            int valor = st.executeUpdate(sql);
+            if (valor>0){
+                resultado = true;
+            }
+            objConec.conex.close();
+            st.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+
+        return resultado;
+    }
+
+    @Override
+    public String consultartercero(int id) {
+        String resultado = "";
+        
+            try {
+                String sqlConsultar = "Select * from terceros where id = "+ id;
+                objConec.conectar();
+                Statement st = objConec.conex.createStatement();
+                ResultSet rs = st.executeQuery(sqlConsultar);
+                    while (rs.next()) {
+                        resultado += rs.getString(2) + " - "
+                        + rs.getString(3) + " - "
+                        + rs.getString(4);
+            }
+            //Se cierran las conexiones
+            rs.close();
+            objConec.conex.close();
+        } catch (Exception e) {
+              //  System.out.println("Error: "+ e.getMessage);
+        }
+        
+        return resultado;    }
+
+    @Override
+    public void shoutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
-    Conexion objConect = new Conexion();
+    public ResultSet cargarTercero(){
+        ResultSet resultado = null;
+        try {
+            String sql = "Select nombres, apellidos, telefono from terceros";
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            resultado = st.executeQuery(sql);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error: "+ e.getMessage());
+        }
+        return resultado;
+    }
 
     @Override
     public boolean insertarTercero(String nombre, String apellido, String telefono) {
-        
-        
-        
-        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -32,61 +121,11 @@ public class Tercero extends TerceroPOA{
 
     @Override
     public String consultarTercero(int id) {
-        
-        String resultado = "";
-        
-        try {
-            
-            String sqlConsultar = "Select * from terceros where id = " + id;
-            objConect.conectar();
-            Statement st = objConect.conex.createStatement();
-            ResultSet rs = st.executeQuery(sqlConsultar);
-            
-            while (rs.next()) {                
-                resultado += rs.getString(2) + " - " + rs.getString(3) + " - " + rs.getString(4);
-            }
-            
-            //Cerrar las conexiones.
-            rs.close();
-            objConect.conex.close();
-            
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        
-        
-        return resultado;
-        
-        
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void shutdown() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public ResultSet cargarTercero(){
-        
-       ResultSet resultado = null;
-       
-        try {
-            String sqlConsultar = "Select nombres, apellidos, telefono from terceros";
-            objConect.conectar();
-            Statement st = objConect.conex.createStatement();
-            resultado = st.executeQuery(sqlConsultar);
-           
-            //Cerrar las conexiones.
-           // rs.close();
-            objConect.conex.close();
-            
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error " + e.getMessage());
-        }
-       return resultado;
-        
-        
-    }
-    
-    
 }
